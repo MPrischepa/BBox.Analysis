d8ea57783df9e5f2df1494db2f1294c3d2e8364a
MPrischepa <m.a.prischepa@gmail.com>
2018-02-11 16:07:00 +0500

    Заебенить заебись

---
+++ GasStationReports.FileManager/GasStationReports.FileManager/ArchiveEnumerable.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Ionic.Zip;
+
+namespace GasStationReports.FileManager
+{
+    internal class ArchiveEnumerable : IEnumerable<FileContent>
+    {
+        private String _fileName;
+
+        public ArchiveEnumerable(String fileName)
+        {
+            _fileName = fileName;
+        }
+
+        #region Implementation of IEnumerable
+
+        /// <summary>Returns an enumerator that iterates through the collection.</summary>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
+        public IEnumerator<FileContent> GetEnumerator()
+        {
+           return new ArchiveEnumerator(_fileName);
+        }
+
+        /// <summary>Returns an enumerator that iterates through a collection.</summary>
+        /// <returns>An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        class ArchiveEnumerator : IEnumerator<FileContent>
+        {
+            private ZipFile _zipFile;
+            private IEnumerator<ZipEntry> _entryEnum;
+            private bool _isDisposed;
+
+            public ArchiveEnumerator(String fileName)
+            {
+                _zipFile = ZipFile.Read(fileName);
+                _entryEnum = _zipFile.GetEnumerator();
+            }
+
+            #region Implementation of IDisposable
+
+            /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
+            public void Dispose()
+            {
+                if (!_isDisposed)
+                {
+                    _entryEnum?.Dispose();
+                    _zipFile?.Dispose();
+                    _isDisposed = true;
+                }
+                _entryEnum = null;
+                _zipFile = null;
+            }
+
+
+            #endregion
+
+            #region Implementation of IEnumerator
+
+            /// <summary>Advances the enumerator to the next element of the collection.</summary>
+            /// <returns>true if the enumerator was successfully advanced to the next element; false if the enumerator has passed the end of the collection.</returns>
+            /// <exception cref="T:System.InvalidOperationException">The collection was modified after the enumerator was created. </exception>
+            public bool MoveNext()
+            {
+                return _entryEnum.MoveNext();
+            }
+
+            /// <summary>Sets the enumerator to its initial position, which is before the first element in the collection.</summary>
+            /// <exception cref="T:System.InvalidOperationException">The collection was modified after the enumerator was created. </exception>
+            public void Reset()
+            {
+                _entryEnum.Reset();
+            }
+
+            /// <summary>Gets the element in the collection at the current position of the enumerator.</summary>
+            /// <returns>The element in the collection at the current position of the enumerator.</returns>
+            public FileContent Current
+            {
+                get
+                {
+                    var __memoryStream = new MemoryStream();
+                    //var __writeStream = File.Create(Path.Combine(outputDir,__entry.FileName));
+                    Debug.Assert(_entryEnum.Current != null, "_entryEnum.Current != null");
+                    _entryEnum.Current.Extract(__memoryStream);
+                    var __stream = new StreamReader(__memoryStream, Encoding.GetEncoding(1251));
+                    __stream.BaseStream.Position = 0;
+                    return new FileContent
+                    {
+                        FileName = _entryEnum.Current.FileName,
+                        Content = __stream.ReadToEnd(),
+                    };
+                }
+            }
+
+            /// <summary>Gets the current element in the collection.</summary>
+            /// <returns>The current element in the collection.</returns>
+            object IEnumerator.Current => Current;
+
+            #endregion
+        }
+    }
+
+    internal class FileContent
+    {
+        public String FileName { get; set; }
+
+        public String Content { get; set; }
+    }
+}
---
+++ GasStationReports.FileManager/TemplateTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TemplateTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EuropaDateTimeTemplateTest()
+        {
+            var __inputStr =
+                "1088;10.02.2018 8:00:47;AZS37;11.02.2018 2:02:00;Установлено время 11.02.2018 2:02:00; расчитанное время 11.02.2018 1:00:17;18";
+            var __searchPattern = "(\\d{2}).(\\d{2}).(\\d{4})\\s(\\d{1,2}):(\\d{2}):(\\d{2})";
+            var __outputStr = Regex.Replace(__inputStr, __searchPattern, "$3-$2-$1 $4:$5:$6");
+            Assert.AreEqual(__outputStr, "1088;2018-02-10 8:00:47;AZS37;2018-02-11 2:02:00;Установлено время 2018-02-11 2:02:00; расчитанное время 2018-02-11 1:00:17;18");
+
+            __searchPattern = "(\\d{4}-\\d{2}-\\d{2})\\s(\\d{1}):(\\d{2}:\\d{2})";
+            __outputStr = Regex.Replace(__outputStr, __searchPattern, "$1 0$2:$3");
+            Assert.AreEqual(__outputStr, "1088;2018-02-10 08:00:47;AZS37;2018-02-11 02:02:00;Установлено время 2018-02-11 02:02:00; расчитанное время 2018-02-11 01:00:17;18");
+        }
+    }
+}
